f051ac9d4a87a68c9efa2fa5778b724f40e3d988
azjezz <azjezz@protonmail.com>
2026-03-17 06:21:35 +0100

    chore: move components/ to packages/
    Signed-off-by: azjezz <azjezz@protonmail.com>

---
+++ src/Psl/Iter/contains_key.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\Iter;
+
+/**
+ * Returns true if the given iterable contains the key.
+ *
+ * @template Tk
+ * @template Tv
+ *
+ * @param iterable<Tk, Tv> $iterable
+ * @param Tk $key
+ */
+function contains_key(iterable $iterable, mixed $key): bool
+{
+    if (is_array($iterable) && (is_int($key) || is_string($key))) {
+        return array_key_exists($key, $iterable);
+    }
+
+    foreach ($iterable as $k => $_) {
+        if ($key === $k) {
+            return true;
+        }
+    }
+
+    return false;
+}
